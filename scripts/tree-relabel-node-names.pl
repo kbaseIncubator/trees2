adb14f79987695459c94fb4750889c22588ea169
msneddon <mwsneddon@lbl.gov>
2013-02-04 14:14:37 -0800

    updated tree-relabel script

--- scripts/tree-relabel-node-names.pl
+++ scripts/tree-relabel-node-names.pl
@@ -14,225 +14,160 @@ NAME
       tree-relabel-node-names -- allows you to relabel node names in a newick tree
 
 SYNOPSIS
-      tree-relabel-node-names -t newick_tree -r=replacement_names [--input=inputFileName --output=outputFileName]
+      tree-relabel-node-names [OPTIONS] [NEWICK_TREE]
 
 DESCRIPTION
-     Given a tree in newick format, relabel the specified nodes with replacement names.
-
-
-      -t, --tree
-                        a string in newick format
+     Given a tree in newick format, relabel the specified nodes with replacement names found in
+     a mappingfile.  If the tree is not provided as an argument and no input file is specified,
+     the tree is read in from standard-in.
       
-      -r, --replacement
-                        specify the names to relabel and thier replacements
+      -r, --replacement-file
+                        specify the file name of the replacement mappings; this file should
+                        be a two column file, with columns delimited by tabs, where the first
+                        column indicates names to search for in the tree, and the second column
+                        indicates the replacement string; the replacement string can be blank;
                         
       -i, --input
                         specify an input file to read the tree from
-      -o, --output
-                        specify an output file to write the results to
+                        
       -h, --help
                         diplay this help message, ignore all arguments
                         
                         
 EXAMPLES
-      Retrieve all the leaf nodes of a given newick tree
-      > tree-replace-node-names -t='(l1,((l2,l3)n2,(l4,l5)n3)n1)root;\tl1\tl55' -r='l1\tnode'
-      
-AUTHORS
-      Matt Henderson (mhenderson\@lbl.gov)
-      Michael Sneddon (mwsneddon\@lbl.gov)
+      Replace node names based on the file mapping.txt
+      > cat mapping.txt
+      l1	r1
+      l2	mr_tree
+      l3	
+      > tree-relabel-node-names -r test_mapping_file.txt '(l1,l2,l3,l4)root;'
+      (r1,mr_tree,,l4)root;
 
-      
+AUTHORS
+      Michael Sneddon (mwsneddon\@lbl.gov)
+      Matt Henderson (mhenderson\@lbl.gov)
+
 ";
 
-my $n_args = $#ARGV + 1;
 
 my $help = '';
-my $treeString = '';
-my $replacementString = '';
-
+my $treeString='';
 my $inputFile = '';
-my $outputFile = '';
+my $replacementFile = '';
 
+# parse arguments and output file
 my $stdinString = "";
-
 my $opt = GetOptions("help" => \$help,
                      "input=s" => \$inputFile,
-                     "output=s" => \$outputFile,
-                     "tree=s" => \$treeString,
-                     "replacement=s" => \$replacementString);
-
+                     "replacement-file=s" => \$replacementFile
+                     );
 if($help) {
      print $DESCRIPTION;
      exit 0;
 }
-
-if($n_args == 0) {
-    while(my $line = <STDIN>) {
-        $n_args = 1;
-        $stdinString = $stdinString.$line;
-    }
-
-    if ($n_args == 0) {
-        print "FAILURE - no feature or sequence specified.  Run with --help for usage.\n";
-        exit 1;
-    }
-} 
-
-
-#create client
-my $treeClient;
-
-eval{ $treeClient = get_tree_client(); };
-
-my $client_error = $@;
-if ($client_error) { print Dumper($client_error);  exit 1;}
-
-if(!$treeClient) {
-    print "FAILURE - unable to create tree service client.  Is your tree URL correct? see tree-url.\n";
-    exit 1;
+if(!$replacementFile) {
+     print "FAILURE - no replacement mapping provided.  Run with -h for usage.\n";
+     exit 1;
 }
 
 
-if ($n_args == 1) {
-        my $inputFileHandle;
-        if ($inputFile) {
-            open($inputFileHandle, "<", $inputFile) or die "Cannot open $inputFile: $!";
-        }
-        else {
-            $inputFileHandle = \*STDIN;
-        }
-
-        my $inputString = $stdinString;
-
-        eval {
-            while (my $line = <$inputFileHandle>) {
-                $inputString = $inputString.$line;
-            }
-            close $inputFileHandle;
-        };
-
-        my @inputList = split('\t', $inputString);
-
-        $treeString = shift(@inputList);
-        $replacementString = join('=>', @inputList);
-
-        my %replacements;
-
-        my $i;
-        for($i = 0; $i < @inputList; $i+=2) {
-            $replacements{$inputList[$i]} = $inputList[$i+1];
-        }
-
-        my $new_tree;
-        eval {
-            $new_tree = $treeClient->replace_node_names($treeString, \%replacements);
-        };
-
-        $client_error = $@;
-        if ($client_error) {
-            print Dumper($client_error);
-            exit 1;
-        }
-
-        print $new_tree."\t";
-        exit 0;
+my $n_args = $#ARGV + 1;
+# if we have specified an input file, then read the file
+if($inputFile) {
+     my $inputFileHandle;
+     open($inputFileHandle, "<", $inputFile);
+     if(!$inputFileHandle) {
+          print "FAILURE - cannot open '$inputFile' \n$!\n";
+          exit 1;
+     }
+     eval {
+          while (my $line = <$inputFileHandle>) {
+               chomp($line);
+               $treeString = $treeString.$line;
+          }
+          close $inputFileHandle;
+     };
 }
-elsif ($n_args == 2) {
-    
-    if($treeString && $replacementString) {
-        my %replacements;
 
-        $replacementString = substr($replacementString, 1, -1);
-        my @replacementList = split(',', $replacementString);
-
-        my @hash_elements;
-        foreach (@replacementList) {
-            @hash_elements = split('=>', $_);
-            $replacements{$hash_elements[0]} = $hash_elements[1];
-        }
-
-        my $new_tree;
-        eval {
-            $new_tree = $treeClient->replace_node_names($treeString, \%replacements);
-        };
-
-        $client_error = $@;
-        if ($client_error) {
-            print Dumper($client_error);
-            exit 1;
-        }
-
-        print $new_tree."\t";
-        exit 0;
-    } 
-    elsif ($inputFile && $outputFile) {
-        my $inputFileHandle;
-        if ($inputFile) {
-            open($inputFileHandle, "<", $inputFile) or die "Cannot open $inputFile: $!";
-        }
-        else {
-            $inputFileHandle = \*STDIN;
-        }
-
-        my $treeString = $stdinString;
-
-        eval {
-            while (my $line = <$inputFileHandle>) {
-                $treeString = $treeString.$line;
-            }
-            close $inputFileHandle;
-        };
-
-        $client_error = $@;
-        if ($client_error) {
-            print Dumper($client_error);
-            exit 1;
-        }
-
-        my %replacements;
-
-        $replacementString = substr($replacementString, 1, -1);
-        my @replacementList = split(',', $replacementString);
-
-        my @hash_elements;
-        foreach (@replacementList) {
-            @hash_elements = split('=>', $_);
-            $replacements{$hash_elements[0]} = $hash_elements[1];
-        }
-
-        $client_error = $@;
-        if ($client_error) {
-            print Dumper($client_error);
-            exit 1;
-        }
-
-        my $new_tree;
-        eval {
-            $new_tree = $treeClient->replace_node_names($treeString, %replacements);
-        };
-
-        $client_error = $@;
-        if ($client_error) {
-            print Dumper($client_error);
-            exit 1;
-        }
-
-        my $outputFileHandle;
-        if ($outputFile) {
-            open($outputFileHandle, ">", $outputFile) or die "Cannot open $outputFile: $!";
-        }
-        else {
-            $outputFileHandle = \*STDOUT;
-        }
-
-        print $outputFileHandle $new_tree."\t";
-        close($outputFileHandle);
-        exit 0;
-    }        
+# if we have a single argument, then accept it as the treeString
+elsif($n_args==1) {
+     $treeString = $ARGV[0];
+     chomp($treeString);
 }
+
+# if we have no arguments, then read the tree from standard-in
+elsif($n_args == 0) {
+     while(my $line = <STDIN>) {
+          chomp($line);
+          $treeString = $treeString.$line;
+     }
+}
+
+# otherwise we have some bad number of commandline args
 else {
     print "Bad options / Invalid number of arguments.  Run with --help for usage.\n";
     exit 1;
 }
+
+#create client
+my $treeClient;
+eval{ $treeClient = get_tree_client(); };
+my $client_error = $@;
+if ($client_error) {
+     print Dumper($client_error);
+     print "FAILURE - unable to create tree service client.  Is you tree URL correct? see tree-url.\n";
+     exit 1;
+}
+
+# make sure we got something out of all of that
+if ($treeString ne '') {
+     
+     my $replacementMapping = {};
+     my $inputFileHandle;
+     open($inputFileHandle, "<", $replacementFile);
+     if(!$inputFileHandle) {
+          print "FAILURE - cannot open replacement file '$inputFile' \n$!\n";
+          exit 1;
+     }
+     eval {
+          my $line_number =0;
+          while (my $line = <$inputFileHandle>) {
+               $line_number++;
+               chomp($line);
+               if($line eq '') { next; }
+               my @tokens = split("\t",$line);
+               if(scalar (@tokens) > 2) {
+                    print "FAILURE - malformed replacement file input on line $line_number.  Only two tab delimited columns permitted.\n";
+                    exit 1;
+               }
+               if(scalar (@tokens) == 2) {
+                    $replacementMapping->{$tokens[0]}=$tokens[1];
+               } elsif(scalar (@tokens) == 1) {
+                    $replacementMapping->{$tokens[0]}='';
+               }
+          }
+          close $inputFileHandle;
+     };
+     my $new_tree;
+     eval {
+          $new_tree = $treeClient->replace_node_names($treeString, $replacementMapping);
+     };
+
+     $client_error = $@;
+     if ($client_error) {
+          print Dumper($client_error);
+          print "FAILURE - error calling Tree service.\n";
+          exit 1;
+     }
+
+     print $new_tree."\n";
+     exit 0;
+     
+} else {
+     print "FAILURE - no tree specified.  Run with --help for usage.\n";
+     exit 1;
+}
+
+exit 0;
+
 
