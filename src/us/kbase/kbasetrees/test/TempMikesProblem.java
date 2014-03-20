79e0aad7467b1453bd02cfa8b7899f4a6ada02c8
Roman Sutormin <rsutormin@lbl.gov>
2014-03-20 11:45:41 -0700

    For testing perl server connection problem.

--- src/us/kbase/kbasetrees/test/TempMikesProblem.java
+++ src/us/kbase/kbasetrees/test/TempMikesProblem.java
@@ -3,14 +3,28 @@ package us.kbase.kbasetrees.test;
 import java.net.URL;
 import java.util.List;
 
+import org.junit.Test;
+
+import us.kbase.kbasetrees.KBaseTreesClient;
 import us.kbase.tree.TreeClient;
 
 public class TempMikesProblem {
+
 	public static void main(String[] args) throws Exception {
-		for (int i = 0; i < 100; i++) {
+		new TempMikesProblem().testPerlConnectionBug();
+	}
+	
+	@Test
+	public void testPerlConnectionBug() throws Exception {
+		for (int i = 0; i < 20; i++) {
 			TreeClient cl = new TreeClient(new URL("http://140.221.84.184:7047"));
 			List<String> ret = cl.extractLeafNodeNames("(k);");
-			System.out.println(i + "\t" + ret);
+			//System.out.println(i + "\t" + ret);
+		}
+		for (int i = 0; i < 20; i++) {
+			KBaseTreesClient cl = new KBaseTreesClient(new URL("http://140.221.85.58:8284/"));
+			List<String> ret = cl.extractLeafNodeNames("(k);");
+			//System.out.println(i + "\t" + ret);
 		}
 	}
 }
