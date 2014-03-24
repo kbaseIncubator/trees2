cd139340f7ef31d9cd82992dfc958f838f3b72ba
msneddon <mwsneddon@lbl.gov>
2014-03-24 13:14:31 -0700

    reverted so that stop_service will stop domain

--- lib/KBTree_cpp_lib/lib/java_interface/lib-loader/us/kbase/kbasetrees/cpputil/KBTreeUtilLibLoader.java
+++
@@ -1,7 +0,0 @@
-package us.kbase.kbasetrees.cpputil;
-public class KBTreeUtilLibLoader {
-	static {
-        System.loadLibrary("KBTreeUtil");
-    }
-	static public String doubleCheck() { return "yes, KBTreeUtilLoader here.\n"; }
-}
